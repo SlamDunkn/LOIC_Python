880ea9ed44a75faaf94ed42ae8497182f1bdb2af
noyx <lhall@phiral.net>
2010-12-22 08:42:54 -0500

    Automate building synmod.so on start

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
-python main.py irc.hiddenaces.net 6667 \#loic
+#!/bin/bash
+if [ ! -f Core/synmod.so ]; then
+    cd SYNModule
+    ./build.sh
+    mv synmod.so ../Core/
+fi
+
+#python main.py irc.hiddenaces.net 6667 \#loic
